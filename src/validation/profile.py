ea485e8f9c1de0d1a562c9dff745f9739762c1e0
martoff <martoff@ukr.net>
2026-07-31 21:49:01 +0300

    applay raise HTTPException to all validate functions

--- src/validation/profile.py
+++ src/validation/profile.py
@@ -3,15 +3,18 @@ from datetime import date
 from io import BytesIO
 
 from PIL import Image
-from fastapi import UploadFile
+from fastapi import UploadFile, status, HTTPException
 
 from database.models.accounts import GenderEnum
 
 
 def validate_name(name: str):
     if re.search(r'^[A-Za-z]*$', name) is None:
-        raise ValueError(f'{name} contains non-english letters')
-
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"{name} contains non-english letters"
+        )
+    
 
 def validate_image(avatar: UploadFile) -> None:
     supported_image_formats = ["JPG", "JPEG", "PNG"]
@@ -19,27 +22,49 @@ def validate_image(avatar: UploadFile) -> None:
 
     contents = avatar.file.read()
     if len(contents) > max_file_size:
-        raise ValueError("Image size exceeds 1 MB")
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Image size exceeds 1 MB"
+        )
 
     try:
         image = Image.open(BytesIO(contents))
         avatar.file.seek(0)
         image_format = image.format
         if image_format not in supported_image_formats:
-            raise ValueError(f"Unsupported image format: {image_format}. Use one of next: {supported_image_formats}")
+            raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Unsupported image format: {image_format}. Use one of next: {supported_image_formats}"
+        )
+
     except IOError:
-        raise ValueError("Invalid image format")
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Invalid image format"
+        )
+
 
 
 def validate_gender(gender: str) -> None:
-    if gender not in GenderEnum.__members__.values():
-        raise ValueError(f"Gender must be one of: {', '.join(g.value for g in GenderEnum)}")
+    if gender.lower() not in [g.value for g in GenderEnum]:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=f"Gender must be one of: {', '.join(g.value for g in GenderEnum)}"
+        )
 
 
 def validate_birth_date(birth_date: date) -> None:
     if birth_date.year < 1900:
-        raise ValueError('Invalid birth date - year must be greater than 1900.')
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Invalid birth date - year must be greater than 1900."
+        )
+
 
     age = (date.today() - birth_date).days // 365
     if age < 18:
-        raise ValueError('You must be at least 18 years old to register.')
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="You must be at least 18 years old to register."
+        )
+
